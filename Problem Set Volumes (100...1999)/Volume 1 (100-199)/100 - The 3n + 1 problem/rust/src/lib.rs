4986851fb4c4168224238fad6bb61286b740d430
jonmsawyer <jonmsawyer@gmail.com>
2020-05-30 14:23:49 -0800

    I now have a properly running CLI application.

--- Problem Set Volumes (100...1999)/Volume 1 (100-199)/100 - The 3n + 1 problem/rust/src/lib.rs
+++ Problem Set Volumes (100...1999)/Volume 1 (100-199)/100 - The 3n + 1 problem/rust/src/lib.rs
@@ -1,12 +1,60 @@
+//! rust crate
+//! Author: Jonathan Sawyer <jonmsawyer[at]gmail.com>
+
+#[derive(Debug)]
+pub struct Config<'a> {
+    pub inputs: Vec<&'a str>,
+    pub i: u32,
+    pub j: u32,
+}
+
+impl<'a> Config<'a> {
+    pub fn new(input: &'a str) -> Result<Config<'a>, String> {
+        let inputs: Vec<&str> = input.split_whitespace().collect();
+        
+        if inputs.len() > 2 {
+            return Err(
+                format!("too many input parameters, expected 2, got {}", inputs.len())
+            );
+        }
+        
+        if inputs.len() < 2 {
+            return Err(
+                format!("too few input parameters, expected 2, got {}", inputs.len())
+            );
+        }
+        
+        let i = match inputs[0].parse::<u32>() {
+            Ok(number) => number,
+            Err(e) => return Err(format!("error: {:?}", e)),
+        };
+        
+        let j = match inputs[1].parse::<u32>() {
+            Ok(number) => number,
+            Err(e) => return Err(format!("error: {:?}", e)),
+        };
+        
+        Ok(Config { inputs, i, j })
+    }
+}
+
+/// Returns a `Vec<u32>` containing the elements of the cycle length
+/// of `n` with respect to the "3n + 1 problem".
+///
 /// Consider the following algorithm:
+///
 ///  1. input n
 ///  2. print n
 ///  3. if n = 1 then STOP
-///  4. if n is odd then n ←− 3n + 1
-///  5. else n ←− n/2
+///  4. if n is odd then n ← 3n + 1
+///  5. else n ← n/2
 ///  6. GOTO 2
-/// Given the input 22, the following sequence of numbers will be printed
-/// 22 11 34 17 52 26 13 40 20 10 5 16 8 4 2 1
+///
+/// Given the input 22, the following sequence of numbers will be:
+///
+/// ```
+/// vec![22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
+/// ```
 pub fn cycles(mut n: u32) -> Vec<u32> {
     let mut cycle_vec = Vec::<u32>::new();
     loop {
@@ -25,6 +73,18 @@ pub fn cycles(mut n: u32) -> Vec<u32> {
         }
     }
     cycle_vec
+}
+
+pub fn max_cycles(config: Config) -> (u32, u32, u32) {
+    let mut max: usize = 0;
+    
+    for n in config.i..=config.j {
+        let cycle_vec = cycles(n);
+        max = cycle_vec.len().max(max);
+        //println!("cycle_vec = {:?}, cycle length = {}", cycle_vec, cycle_vec.len());
+    }
+    
+    (config.i, config.j, max as u32)
 }
 
 #[cfg(test)]
--- Problem Set Volumes (100...1999)/Volume 1 (100-199)/100 - The 3n + 1 problem/rust/src/main.rs
+++ Problem Set Volumes (100...1999)/Volume 1 (100-199)/100 - The 3n + 1 problem/rust/src/main.rs
@@ -1,7 +1,33 @@
-use rust::cycles;
+use std::io;
 
-fn main() {
-    let n = 22;
-    let cycle_vec = cycles(n);
-    println!("cycle_vec = {:?}, cycle length = {}", cycle_vec, cycle_vec.len());
+use rust::{Config, max_cycles};
+
+fn main() -> io::Result<()> {
+    loop {
+        let mut input = String::new();
+        
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                //print!("{} bytes read, input = {}", n, input);
+            }
+            Err(error) => eprintln!("error: {}", error),
+        }
+        
+        let config = match Config::new(&input) {
+            Ok(config) => config,
+            Err(_) => {
+                eprintln!(">> Error: could not parse input into a pair of integers.");
+                Config {
+                    inputs: Vec::<&str>::new(),
+                    i: 0,
+                    j: 0,
+                }
+            },
+        };
+        
+        let (i, j, result) = max_cycles(config);
+        println!("{} {} {}", i, j, result);
+    }
+    
+    //Ok(())
 }
