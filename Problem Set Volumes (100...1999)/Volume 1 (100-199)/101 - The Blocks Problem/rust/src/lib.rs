8264ec36a92b48a1513b797ffaf2f1d95698807a
jonmsawyer <jonmsawyer@gmail.com>
2020-05-31 12:54:18 -0800

    Getting some tests working with the new Blocks struct

--- Problem Set Volumes (100...1999)/Volume 1 (100-199)/101 - The Blocks Problem/rust/src/lib.rs
+++ Problem Set Volumes (100...1999)/Volume 1 (100-199)/101 - The Blocks Problem/rust/src/lib.rs
@@ -4,14 +4,39 @@
 //!
 //! Date: 2020-05-30
 
+//use std::error::Error;
 
+#[derive(Debug, PartialEq)]
+pub struct Blocks(Vec<(u32, Vec<u32>)>);
+
+impl Blocks {
+    pub fn new(elements: u32) -> Result<Blocks, String> {
+        if elements < 1 {
+            return Err("cannot initialize Blocks with less than 1 element".to_string());
+        }
+        
+        let mut vec_elements = Vec::<(u32, Vec<u32>)>::new();
+        for i in 0..elements {
+            vec_elements.push((i as u32, vec![i as u32]));
+        }
+        
+        Ok(Blocks(vec_elements))
+    }
+}
 
 #[cfg(test)]
 mod test {
     use super::*;
     
     #[test]
-    fn it_works() {
-        assert_eq!(1 + 1, 2);
+    fn blocks_init_new() {
+        let blocks = match Blocks::new(3) {
+            Ok(blocks) => blocks,
+            Err(_error) => Blocks(vec![(0, vec![0])]),
+        };
+        assert_eq!(
+            Blocks(vec![(0, vec![0]), (1, vec![1]), (2, vec![2])]),
+            blocks
+        );
     }
 }
--- Problem Set Volumes (100...1999)/Volume 1 (100-199)/101 - The Blocks Problem/rust/src/main.rs
+++ Problem Set Volumes (100...1999)/Volume 1 (100-199)/101 - The Blocks Problem/rust/src/main.rs
@@ -1,3 +1,9 @@
+//use rust::Blocks;
+
 fn main() {
+    //let mut blocks = Blocks(vec![vec![0], vec![1], vec![2]]);
+    //blocks.move(1).onto(3);
+    //blocks.pile(3).over(2);
+    //println!("blocks = {:?}", blocks);
     println!("Hello, world!");
 }
