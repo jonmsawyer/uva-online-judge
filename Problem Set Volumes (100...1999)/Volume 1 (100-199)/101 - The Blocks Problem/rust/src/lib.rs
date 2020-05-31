cdbe1054be394dc66f95ded737afc6cba6ed110b
jonmsawyer <jonmsawyer@gmail.com>
2020-05-30 19:35:18 -0800

    Initial commit for Problem 101

---
+++ Problem Set Volumes (100...1999)/Volume 1 (100-199)/101 - The Blocks Problem/rust/src/lib.rs
@@ -0,0 +1,17 @@
+//! `rust` crate.
+//!
+//! Author: Jonathan Sawyer <jonmsawyer[at]gmail.com>
+//!
+//! Date: 2020-05-30
+
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    
+    #[test]
+    fn it_works() {
+        assert_eq!(1 + 1, 2);
+    }
+}
